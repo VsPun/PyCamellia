2a476dcd42f10d5cb98ee315908e876f558dc82a
Nate Roberts <nvroberts@anl.gov>
2015-02-21 15:50:49 -0600

    Added PyCamellia.py.

---
+++ cs335/phase1/collected_swig_interface/PyCamellia.py
@@ -0,0 +1,17 @@
+from BC import *
+from BF import *
+from Camellia import *
+from Function import *
+from HDF5Exporter import *
+from IP import *
+from LinearTerm import *
+from Mesh import *
+from MeshFactory import *
+from PoissonFormulation import *
+from RHS import *
+from Solution import *
+from SpatialFilter import *
+from StokesVGPFormulation import *
+from Var import *
+from VarFactory import *
+
