a3411d4bcb5ae80b541d8d26ab9ab0eb63e81ad4
Nathan V. Roberts <nvroberts@alcf.anl.gov>
2015-02-23 15:29:36 -0600

    Made StokesCavityFlowExample use heaviside function.

--- example/StokesCavityFlowExample.py
+++ example/StokesCavityFlowExample.py
@@ -6,7 +6,7 @@ dims = [1.0,1.0]
 numElements = [2,2]
 x0 = [0.,0.]
 meshTopo = MeshFactory.rectilinearMeshTopology(dims,numElements,x0)
-polyOrder = 2
+polyOrder = 3
 delta_k = 1
 
 form.initializeSolution(meshTopo,polyOrder,delta_k)
@@ -16,9 +16,18 @@ form.addZeroMeanPressureCondition()
 topBoundary = SpatialFilter.matchingY(1.0)
 notTopBoundary = SpatialFilter.negatedFilter(topBoundary)
 
-x2 = Function.xn(2)
+x = Function.xn(1)
+rampWidth = 1./64
+deltaUp = Function.heaviside(rampWidth)
+deltaDown = Function.heaviside(1.0-rampWidth);
+ramp = (1-deltaDown) * deltaUp + (1./rampWidth) * (1-deltaUp) * x + (1./rampWidth) * deltaDown * (1-x)
+print(ramp.evaluate(0.0,1.0))
+print(ramp.evaluate(0.5,1.0))
+print(ramp.evaluate(1.0/128.0,1.0))
+print(ramp.evaluate(1-1.0/128.0,1.0))
+
 zero = Function.constant(0)
-topVelocity = Function.vectorize(4*x2-1,zero)
+topVelocity = Function.vectorize(ramp,zero)
 
 form.addWallCondition(notTopBoundary)
 form.addInflowCondition(topBoundary,topVelocity)
@@ -30,7 +39,7 @@ energyError = form.solution().energyErrorTotal()
 print("Energy error after %i refinements: %0.3f" % (refinementNumber, energyError))
 
 threshold = 0.2
-while energyError > threshold:
+while energyError > threshold and refinementNumber <= 8:
   form.refine()
   form.solve()
   energyError = form.solution().energyErrorTotal()
